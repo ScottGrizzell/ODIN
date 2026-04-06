c7a50748db3e26c6eef5598398d754b14b7bfe98
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-04-05 17:08:00 -0700

    Added controller for accessing mock data

---
+++ ODIN/Server/Controllers/OffenderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Interfaces;
+using Server.Services;
+using Shared;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OffenderController : ControllerBase
+    {
+        private readonly IOffenderService _offenderService;
+
+        public OffenderController(IOffenderService offenderService)
+        {
+            {
+                _offenderService = offenderService;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OffenderDto>> GetOffender(int id)
+        {
+            var offender = await _offenderService.GetOffenderByIdAsync(id);
+            if(offender == null)
+            {
+                return NotFound();
+            }
+            return Ok(offender);
+        }
+        
+    }
+}
--- ODIN/Server/Program.cs
+++ ODIN/Server/Program.cs
@@ -1,7 +1,11 @@
+using Server.Interfaces;
+using Server.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IOffenderService, MockOffenderService>();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
