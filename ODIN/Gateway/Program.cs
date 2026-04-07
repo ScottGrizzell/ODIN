552422692f4dd12e93aa23cb25978b4a40ec05c0
Scott Grizzell <scottindiegrizzell@gmail.com>
2026-04-06 18:04:36 -0700

    setting up CORS for future frontend

--- ODIN/Gateway/Program.cs
+++ ODIN/Gateway/Program.cs
@@ -2,8 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendPolicy", policy =>
+    {
+        policy.WithOrigins("https://localhost:7241")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("FrontendPolicy");
 app.MapReverseProxy();
 
 app.Run();
