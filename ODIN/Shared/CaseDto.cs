1b794ff66b927ab338efe36157e458bd760d6d0d
Scott Grizzell <scottindiegrizzell@gmail.com>
2026-04-07 14:29:19 -0700

    Added CaseDto object

---
+++ ODIN/Shared/CaseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class CaseDto
+    {
+        public int Id { get; set; } // primary key
+        public int OffenderId { get; set; } // foreign key
+        public string CaseNumber { get; set; } = string.Empty;
+        public string CaseDescription { get; set; } = string.Empty;
+        public string Status { get; set; } = "Open";
+        public DateTime FiledDate { get; set; }
+    }
+}
