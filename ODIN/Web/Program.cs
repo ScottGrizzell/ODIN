d1156dfba94e239d37484dfd8816559b03e0f6f0
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-04-07 11:54:08 -0700

    Rough search page added

--- ODIN/Web/Program.cs
+++ ODIN/Web/Program.cs
@@ -8,11 +8,13 @@ various justice involved individuals.
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Web;
+using Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7152") });
+builder.Services.AddScoped<Offenders>();
 
 await builder.Build().RunAsync();
