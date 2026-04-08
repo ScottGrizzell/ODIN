18c173cbc2cef97da1f5af6f6876b8d63282a319
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-04-08 12:18:43 -0700

    Gateway config has cluster for "third party" API

--- ODIN/Legacy.EnterpriseOne/Controllers/FeesController.cs
+++ ODIN/Legacy.EnterpriseOne/Controllers/FeesController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shared;
 
 namespace Legacy.EnterpriseOne.Controllers
 {
-    public class FeesController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeesController : ControllerBase
     {
-        public IActionResult Index()
+
+        // This really shouldn't be hard coded data
+        // If there's time I want this to be stored in its own SQL db that can then be --
+        // synced with the local OdinDB to handle paid feeds.
+        private readonly List<FeeDto> _mockFees = new()
+    {
+        new FeeDto { Id = 501, OffenderId = 1, FeeType = "Court Costs", Amount = 150.00m, Balance = 150.00m, DueDate = DateTime.Now.AddDays(30) },
+        new FeeDto { Id = 502, OffenderId = 1, FeeType = "Restitution", Amount = 500.00m, Balance = 250.00m, DueDate = DateTime.Now.AddDays(-10) }
+    };
+
+
+        // TODO eventually add PayFees endpoint
+        [HttpGet("offender/{offenderId}")]
+        public ActionResult<List<FeeDto>> GetFees(int offenderId)
         {
-            return View();
+            return Ok(_mockFees.Where(f => f.OffenderId == offenderId).ToList());
         }
     }
 }
