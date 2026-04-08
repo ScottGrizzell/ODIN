bd651acc7faeed7031944a390fd9b01ac71ab8bb
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-04-08 13:30:01 -0700

    setting up JWT middleware in gateway

--- ODIN/Gateway/Program.cs
+++ ODIN/Gateway/Program.cs
@@ -6,9 +6,31 @@ to different assosicated microservices. Hopefully in the future this will be use
 for talking to mock third party APIs like Enterprise One or the DOR
  */
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // If this project was slightly bigger all this hardcoding could get very annoying
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "ODIN.Identity",
+            ValidateAudience = true,
+            ValidAudience = "ODIN.Users",
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Super_secret_private_key_that_should_never_be_used_in_prod"))
+
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddCors(options =>
 {
@@ -23,6 +45,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors("FrontendPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapReverseProxy();
 
 app.Run();
