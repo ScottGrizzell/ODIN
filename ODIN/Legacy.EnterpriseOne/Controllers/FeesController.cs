7a549f12ed26f3934cb642dd0354a628904b9582
ScottGrizzell <scottindiegrizzell@gmail.com>
2026-04-08 12:15:53 -0700

    mocking fee controller

---
+++ ODIN/Legacy.EnterpriseOne/Controllers/FeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Legacy.EnterpriseOne.Controllers
+{
+    public class FeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ODIN/Shared/FeeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class FeeDto
+    {
+        public int Id { get; set; }
+        public int OffenderId { get; set; }
+        public string FeeType { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; } = "Unpaid"; // This such be a struct or enum with  unpai, paid, collections but brevitiy
+    }
+}
